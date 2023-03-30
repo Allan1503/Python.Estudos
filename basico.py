5dda1d7a25dec363456ce7a09bbb61129ce6fbf9
Allan1503 <allan.152009@hotmail.com>
2023-03-30 16:18:56 -0300

    adicionado comentarios ao codigo

--- basico.py
+++ basico.py
@@ -80,7 +80,7 @@ print(calcular_idade(1990)) # 33
 #                                                                ----------------------
 
 # Variáveis globais:
-# Variáveis globais são definidas for a de qualquer função ou classe e podem ser acessadas de qualquer lugar do programa. 
+#  Variáveis globais são definidas for a de qualquer função ou classe e podem ser acessadas de qualquer lugar do programa. 
 # No entanto, é uma prática recomendada evitá-las, pois podem causar efeitos colaterais inesperados em funções e classes. 
 # Um exemplo pode ser uma variável que contém as configurações do programa.
 
@@ -133,7 +133,7 @@ print(dados_aluno['idade']) # saída: 20
 
 
 lista_frutas = ['maçã', 'banana', 'laranja']
-lista_frutas.append('abacaxi')
+lista_frutas.append('abacaxi') # adiciona item
 print(lista_frutas) # saída: ['maçã', 'banana', 'laranja', 'abacaxi']
 
 
@@ -156,7 +156,7 @@ print(conjunto1.union(conjunto2)) # saída: {1, 2, 3, 4}
 
 tupla_numeros = (1, 2, 3)
 print(tupla_numeros[1]) # saída: 2
-
+# Lembrando que a contaguem começa do numero 0
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
 
@@ -178,17 +178,19 @@ y = 3
 
 
 # operadores aritméticos
-soma = x + y
-subtracao = x - y
-multiplicacao = x * y
-divisao = x / y
-resto = x % y
+
+soma = x + y    # Resultado = 8
+subtracao = x - y   # Resultado = 2
+multiplicacao = x * y  # Resultado = 15
+divisao = x / y  # Resultado = 1.6666666666666667
+resto = x % y  # Resultado = 2
 
 
 #                                                                ----------------------
 
 
 # operadores de comparação
+
 maior_que = x > y
 menor_que = x < y
 igual_a = x == y
@@ -199,6 +201,7 @@ diferente_de = x != y
 
 
 # operadores lógicos
+
 and_op = (x > 1) and (y > 1)
 or_op = (x > 1) or (y < 1)
 not_op = not(x > 1)
@@ -211,7 +214,7 @@ not_op = not(x > 1)
 # ---------------------------------------------------------------------------------------------------------------------------------------
 
 
-#Estruturas de controle de fluxo: as estruturas de controle de fluxo são usadas para controlar o fluxo de execução do programa. 
+#Estruturas de controle de fluxo: As estruturas de controle de fluxo são usadas para controlar o fluxo de execução do programa. 
 # As principais estruturas de controle de fluxo em Python são condicionais (if-else) e loops (for, while).
 
 # Exemplo:
@@ -361,8 +364,14 @@ class Pessoa:
 
     def apresenta(self):
         print("Olá, meu nome é", self.nome, "e eu tenho", self.idade, "anos.")
+        
+p = Pessoa("João", 30)
+p.apresenta()
+
+# saida esperada: Olá, meu nome é João e eu tenho 30 anos.
 
 # Este exemplo define uma classe 'Pessoanome e 'idadeidade, e um método 'apresentarapresentar que exibe uma mensagem na tela.
+# utiliza o "p" para definir e printar na tela
 
 
 #                                                                ----------------------
@@ -382,25 +391,38 @@ pessoa1.apresentar()
 
 # Exemplo de classes e objetos relacionados:
 
+# Definir a classe Retângulo com a largura e comprimento dados
 class Retangulo:
     def __init__(self, comprimento, largura):
         self.comprimento = comprimento
         self.largura = largura
 
+    # Calcular a área do retângulo
     def calcular_area(self):
         return self.comprimento * self.largura
 
+# Definir a classe Quadrado que herda da classe Retângulo
 class Quadrado(Retangulo):
     def __init__(self, lado):
+        # Chamar o construtor da classe pai com o mesmo valor para largura e comprimento
         super().__init__(lado, lado)
 
+# Criar instância de Retângulo com comprimento 4 e largura 6
 retangulo1 = Retangulo(4, 6)
+# Calcular a área do retângulo
 area_retangulo1 = retangulo1.calcular_area()
+# Imprimir a área do retângulo
 print(f"Área do retângulo: {area_retangulo1}")
 
+# Criar instância de Quadrado com lado 5
 quadrado1 = Quadrado(5)
+# Calcular a área do quadrado
 area_quadrado1 = quadrado1.calcular_area()
+# Imprimir a área do quadrado
 print(f"Área do quadrado: {area_quadrado1}")
+
+
+# saida esperada: Área do retângulo: 24 ; Área do quadrado: 25
 
 #Este exemplo define duas classes 'RetanguloQuadrado, onde 'QuadradoQuadrado é uma subclasse de 'RetanguloRetangulo.
 #  «RetanguloRetangulo tem um método 'calcular_areacalcular_area que calcula a área do retângulo, 
@@ -595,25 +617,38 @@ except ZeroDivisionError:
 # Exemplo:
 
 # automatizando o envio de e-mails em massa
+
+# importando a biblioteca smtplib para enviar e-mails
 import smtplib
+
+# importando a classe MIMEText para formatar o conteúdo do e-mail
 from email.mime.text import MIMEText
 
+# criando uma mensagem de e-mail
 msg = MIMEText("Olá, essa é uma mensagem de teste!")
-msg['Subject'] = "Testando envio de e-mails em massa"
-msg['From'] = "seuemail@empresa.com"
-msg['To'] = "lista_de_destinatarios.txt"
 
+# adicionando um assunto na mensagem
+msg['Subject'] = "Testando envio de e-mails em massa"
+
+# definindo o remetente do e-mail
+msg['From'] = "seuemail@empresa.com"
+
+# lendo a lista de destinatários do arquivo "lista_de_destinatarios.txt"
 with open("lista_de_destinatarios.txt") as f:
     destinatarios = f.readlines()
 
+# configurando as credenciais do servidor SMTP do Gmail
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login("seuemail@empresa.com", "suasenha")
 
+# iterando sobre a lista de destinatários e enviando a mensagem para cada um
 for destinatario in destinatarios:
     server.sendmail("seuemail@empresa.com", destinatario, msg.as_string())
 
+# encerrando a conexão com o servidor SMTP
 server.quit()
+
 
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
@@ -639,25 +674,26 @@ with open('dados.csv', 'wb') as f:
 #Automação de tarefas repetitivas:
 # Baixar arquivos em lote da internet:
 
-import requests
+import requests   # importa o módulo requests para fazer solicitações HTTP
+import smtplib    # importa o módulo smtplib para enviar emails
 
+# URLs dos arquivos a serem baixados
 urls = ['http://example.com/file1.txt', 'http://example.com/file2.txt', 'http://example.com/file3.txt']
+
+# Loop através de todas as URLs
 for url in urls:
-    response = requests.get(url)
-    with open(url.split('/')[-1], 'wb') as f:
-        f.write(response.content)
+    response = requests.get(url)   # envia uma solicitação HTTP GET para a URL
+    with open(url.split('/')[-1], 'wb') as f:  # abre o arquivo local com o mesmo nome da URL final e modo de escrita binária
+        f.write(response.content)   # escreve o conteúdo da resposta HTTP no arquivo local
 
-# Enviar emails automatizados:
+# Configuração do servidor SMTP para enviar emails
+server = smtplib.SMTP('smtp.gmail.com', 587)   # configura o servidor SMTP e a porta
+server.starttls()   # inicia a conexão segura TLS
+server.login('seuemail@gmail.com', 'suasenha')   # faz login no servidor SMTP
 
-import smtplib
-
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login('seuemail@gmail.com', 'suasenha')
-
-mensagem = "Corpo da mensagem"
-server.sendmail("seuemail@gmail.com", "emaildestino@exemplo.com", mensagem)
-server.quit()
+mensagem = "Corpo da mensagem"   # define a mensagem a ser enviada por email
+server.sendmail("seuemail@gmail.com", "emaildestino@exemplo.com", mensagem)   # envia a mensagem por email
+server.quit()   # finaliza a conexão com o servidor SMTP
 
 
 #                                                                ----------------------
@@ -685,11 +721,20 @@ df_processado = ...
 de = 'seuemail@gmail.com'
 para = ['email1@gmail.com', 'email2@gmail.com']
 msg = 'Subject: Arquivo Processado\n\n{}'.format(df_processado.to_string())
+
+# conectar ao servidor de e-mail
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
+
+# fazer login no e-mail
 server.login(de, 'senha_do_email')
+
+# enviar a mensagem
 server.sendmail(de, para, msg)
+
+# desconectar do servidor de e-mail
 server.quit()
+
 
 # Nesse exemplo, utilizamos a biblioteca Pandas para transformar o arquivo CSV em um dataframe, processamos o dataframe e,
 #  em seguida, enviamos o arquivo processado por e-mail para uma lista de destinatários usando a biblioteca smtplib.
@@ -703,20 +748,22 @@ server.quit()
 
 
 # Análise de dados:
-# Importar um arquivo CSV usando a biblioteca pandas e calcular algumas estatísticas básicas:
 
+# Importar a biblioteca pandas com o alias 'pd'
 import pandas as pd
 
+# Ler o arquivo CSV 'dados.csv' e armazenar em um objeto DataFrame chamado 'dados'
 dados = pd.read_csv('dados.csv')
 
-# exibir as primeiras 5 linhas
+# Exibir as primeiras 5 linhas do DataFrame 'dados'
 print(dados.head())
 
-# calcular a média, desvio padrão e mediana de uma coluna
-media = dados['coluna'].mean()
-desvio_padrao = dados['coluna'].std()
-mediana = dados['coluna'].median()
+# Calcular a média, desvio padrão e mediana da coluna 'coluna' do DataFrame 'dados'
+media = dados['coluna'].mean() # calcular a média
+desvio_padrao = dados['coluna'].std() # calcular o desvio padrão
+mediana = dados['coluna'].median() # calcular a mediana
 
+# Exibir os valores calculados
 print('Média:', media)
 print('Desvio padrão:', desvio_padrao)
 print('Mediana:', mediana)
@@ -728,23 +775,39 @@ print('Mediana:', mediana)
 
 import pandas as pd
 
+# Importar e ler dados do arquivo CSV
 data = pd.read_csv('dados.csv')
-data = data.dropna()
-data = data.groupby('nome').mean()
-data.to_csv('dados_processados.csv')
 
+# Remover linhas com valores ausentes (NaN)
+data = data.dropna()
+
+# Agrupar dados pela coluna 'nome' e calcular a média
+data = data.groupby('nome').mean()
+
+# Exportar os dados processados para um novo arquivo CSV
+data.to_csv('dados_processados.csv')
 
 # Visualizar dados com Matplotlib:
 
 import matplotlib.pyplot as plt
 
+# Definir os dados para plotagem do gráfico
 x = [1, 2, 3, 4]
 y = [10, 20, 30, 40]
 
+# Plotar o gráfico com os dados definidos acima
 plt.plot(x, y)
+
+# Definir o nome do eixo x do gráfico
 plt.xlabel('Eixo X')
+
+# Definir o nome do eixo y do gráfico
 plt.ylabel('Eixo Y')
+
+# Definir o título do gráfico
 plt.title('Título do Gráfico')
+
+# Exibir o gráfico
 plt.show()
 
 
@@ -760,16 +823,25 @@ plt.show()
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
+# Definindo as variáveis de entrada (X) e saída (y)
 X = np.array([[1], [2], [3], [4], [5]])
 y = np.array([10, 20, 30, 40, 50])
 
+# Criando um objeto LinearRegression
 model = LinearRegression()
+
+# Treinando o modelo com os dados de entrada e saída
 model.fit(X, y)
 
+# Definindo novos dados de entrada para previsão
 x_test = np.array([[6]])
+
+# Prevendo a saída correspondente aos novos dados de entrada
 y_pred = model.predict(x_test)
 
+# Imprimindo o resultado da previsão
 print(y_pred)
+
 
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
@@ -888,9 +960,13 @@ def search_google(query):
     # extrair os resultados da busca
     results = []
     for item in soup.select(".g"):
+        # extrair o título do resultado da busca
         title = item.select(".r")[0].get_text()
+        # extrair o link do resultado da busca
         link = item.select(".r")[0].find("a")["href"]
+        # extrair a descrição do resultado da busca
         description = item.select(".s")[0].get_text()
+        # adicionar os resultados à lista de resultados
         results.append((title, link, description))
 
     return results
@@ -908,10 +984,13 @@ def generate_report(query, results):
     doc.setFont("Helvetica", 12)
     y = 700
     for title, link, description in results:
+        # adicionar o título do resultado
         doc.drawString(72, y, title)
         doc.setFont("Helvetica-Oblique", 10)
+        # adicionar o link do resultado
         doc.drawString(72, y - 20, link)
         doc.setFont("Helvetica", 12)
+        # adicionar a descrição do resultado
         doc.drawString(72, y - 40, description)
         y -= 80
 
@@ -922,6 +1001,7 @@ def generate_report(query, results):
 query = "python tutorial"
 results = search_google(query)
 generate_report(query, results)
+
 
 
 # Neste exemplo, a função 'search_google recebe uma string contendo o termo a ser pesquisado no Google e 
